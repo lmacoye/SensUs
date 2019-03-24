a456ee46c5f6898a4be0f996eeda2315736d3524
lmacoye <lorenz.macoye@student.uhasselt.be>
2019-03-24 18:06:23 +0100

    Adjusted SerialRead example without layout errors

--- SerialExample.py
+++ SerialExample.py
@@ -1,7 +1,8 @@
 import time
 import serial
 
-# configure the serial connections (the parameters differs on the device you are connecting to)
+# configure the serial connections (the parameters differs on the device you
+# are connecting to)
 ser = serial.Serial(
     port='/dev/ttyUSB1',
     baudrate=9600,
@@ -25,13 +26,15 @@ while 1:
         exit()
     else:
         # send the character to the device
-        # (note that I happend a \r\n carriage return and line feed to the characters - this is requested by my device)
+        # (note that I happend a \r\n carriage return and line feed to the
+        # characters - this is requested by my device)
         ser.write(input + '\r\n')
         out = ''
-        # let's wait one second before reading output (let's give device time to answer)
+        # let's wait one second before reading output (let's give device time
+        # to answer)
         time.sleep(1)
         while ser.inWaiting() > 0:
             out += ser.read(1)
 
         if out != '':
-            print ">>" + out
+            print(">>" + out)
