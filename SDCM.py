e12573ac10421eef3d37536a17cc69d2c6b01ea4
lmacoye <lorenz.macoye@student.uhasselt.be>
2019-05-16 09:54:14 +0200

    Deleted SerialExample

--- SDCM.py
+++ SDCM.py
@@ -1,3 +1,10 @@
 import ctypes
 
 SDCMlib = ctypes.WinDLL("C:\\Users\\loren\\github\\SensUs\\InterfaceDll\\sdcm_std.dll")
+
+print(SDCMlib.SDCM_Init(1, 0))
+
+
+def Initialization():
+    print(SDCMlib.SDCM_Init(0, 0))
+    print(SDCMlib.SDCM_GetNumberOfDetectedElectronics())
