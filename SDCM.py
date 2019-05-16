7bb1e0a0a704430f4b6adbdf13af290449d99fdb
lmacoye <lorenz.macoye@student.uhasselt.be>
2019-05-16 22:41:56 +0200

    Adjustments to SCDM_Init

--- SDCM.py
+++ SDCM.py
@@ -1,8 +1,14 @@
 import ctypes
 
 SDCMlib = ctypes.WinDLL("C:\\Users\\loren\\github\\SensUs\\InterfaceDll\\sdcm_std.dll")
+SDCM_Init = SDCMlib.SDCM_Init
 
-print(SDCMlib.SDCM_Init(1, 0))
+SDCM_Init.argtypes = ctypes.c_int, ctypes.c_int
+SDCM_Init.restype = ctypes.c_int
+ComportNr = ctypes.c_int32(0)
+SearchComport = ctypes.c_int32(0)
+Init_Return = SDCM_Init(ComportNr, SearchComport)
+print(Init_Return)
 
 
 def Initialization():
